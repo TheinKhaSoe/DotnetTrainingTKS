272a9fef2da3f188e2b607ff6487f6f49a866209
Theinkha <TheinkhaSoe@fusionsol.com>
2024-02-18 20:36:56 +0630

    Day1-1

--- DotnetTrainingTKS.ConsoleApp/Program.cs
+++ DotnetTrainingTKS.ConsoleApp/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "THEINKHASOE\\MSSQL2019";
+sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sa@123";
+
+string query = "select * from tbl_Blog";
+SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+sqlConnection.Open();
+SqlCommand cmd = new SqlCommand(query, sqlConnection);    
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+sqlConnection.Close();
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogId"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+}
+
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # DotnetTrainingTKS
+C#
+.net framework(windows)
+1
+2
+3
+4
+4.5
+4.6
+4.7
+4.8
+
+.net core(windows, Linux, macos)
+1
+2
+2.2
+3
+3.1
+.net
+5(vs2019, vs2022)
+6(vscode, vs2022)
+7(current)
+8
+9(beta)
+
+~console app
+~window forms
+~asp.net web forms
+~asp.net core web mvc
+~asp.net core web api
